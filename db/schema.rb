421209a1041b7814fbbddd5c9cd8902c4d725b23
fernandodxx <fernandosillvasantoss@gmail.com>
2024-05-23 22:47:11 -0300

    Add User model and validates

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_23_130805) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_24_014406) do
   create_table "events", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.datetime "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.string "password_confirmation"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
